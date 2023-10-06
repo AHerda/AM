ea6b57609a67124e314c686df6d649b26d888ac5
AHerda <adrianherda@gmail.com>
2023-10-06 23:51:17 +0200

    Działający program

--- lista1/src/main.rs
+++ lista1/src/main.rs
@@ -1,5 +1,49 @@
+use std::{env::args, process, fs};
+
+use vertecies::{Graph, Point};
+
 mod vertecies;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = args().collect();
+    if args.len() != 2 {
+        eprintln!("Wrong number of arguments");
+        process::exit(1);
+    }
+
+    let mut graph = read_file(&args[1]);
+    let mst = graph.mst();
+    println!("{:#?}", mst);
+}
+
+
+fn read_file(path: &String) -> Graph {
+    let content: String = match fs::read_to_string(path) {
+        Ok(s) => s,
+        Err(err) => {
+            eprintln!("{err}");
+            process::exit(1);
+        },
+    };
+    create_graph(&content)
+}
+
+fn create_graph(content: &String) -> Graph {
+    let mut graph: Graph = Graph::new();
+    let mut flag: bool = false;
+    content.split('\n').for_each(|line| {
+        if flag {
+            let numbers: Vec<&str> = line.split(' ').collect();
+            graph.add_point(Point::new(
+                numbers[0].parse::<i32>().unwrap(),
+                numbers[1].parse::<i32>().unwrap(), 
+                numbers[2].parse::<i32>().unwrap()
+            ));
+        }
+        else if line == "NODE_COORD_SECTION" {
+            flag = true;
+        }
+    });
+
+    graph
 }
--- lista1/src/vertecies.rs
+++ lista1/src/vertecies.rs
@@ -1,8 +1,8 @@
 use std::cmp::Ordering;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Point {
-    id: i32,
+    pub id: i32,
     x: i32,
     y: i32,
 }
@@ -17,79 +17,107 @@ impl Point {
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
-struct Edge<'a> {
-    from: &'a Point,
-    to: &'a Point,
-    weight: i32,
-}
-
-impl<'a> Edge<'a> {
-    fn new(from: &'a Point, to: &'a Point) -> Edge<'a> {
-        Edge {
-            from,
-            to,
-            weight: from.distance(to),
-        }
-    }
-}
-
-impl<'a> Ord for Edge<'a> {
+impl<'a> Ord for Point {
     fn cmp(&self, other: &Self) -> Ordering {
-        other.weight.cmp(&self.weight)
+        other.id.cmp(&self.id)
     }
 }
 
-impl<'a> PartialOrd for Edge<'a> {
+impl<'a> PartialOrd for Point {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-pub struct Graph<'a> {
-    edges: Vec<Edge<'a>>,
-    points: Vec<&'a Point>,
+#[derive(Debug, PartialEq, Eq)]
+struct Edge {
+    pub from: i32,
+    pub to: i32,
+    weight: i32,
+}
+
+impl Edge {
+    fn new(from: &Point, to: &Point) -> Edge {
+        Edge {
+            from: from.id,
+            to: to.id,
+            weight: from.distance(to),
+        }
+    }
+}
+
+impl Ord for Edge {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.weight.cmp(&self.weight)
+    }
+}
+
+impl PartialOrd for Edge {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+pub struct Graph {
+    edges: Vec<Edge>,
+    points: Vec<Point>,
     n: i32,
 }
 
-impl<'a> Graph<'a> {
-    pub fn new() -> Graph<'a> {
+impl Graph {
+    pub fn new() -> Graph {
         Graph { edges: Vec::new(), points: Vec::new(), n: 0 }
     }
 
-    pub fn add_point(&mut self, point: &'a Point) {
+    pub fn add_point(&mut self, point: Point) {
         self.points.iter().for_each(|point_temp| {
-            self.edges.push(Edge::new(point, point_temp));
+            self.edges.push(Edge::new(&point, point_temp));
         });
+
+
         self.points.push(point);
         self.n += 1;
     }
 
-    pub fn size(&self) -> i32 {
+    pub fn _size(&self) -> i32 {
         self.n
     }
 
-    pub fn edges(&self) -> i32 {
+    pub fn _edges(&self) -> i32 {
         (self.n * self.n - self.n) / 2
     }
 
-    pub fn sort(&mut self) {
+    pub fn _sort(&mut self) {
         self.edges.sort();
     }
 
-    pub fn minimum_sapnning_tree(&self) -> Vec<Edge> {
+    pub fn mst(&mut self) -> Vec<(i32, i32)> {
         self.edges.sort();
 
-        let mut mst: Vec<Edge> = Vec::new();
+        let mut mst: Vec<(i32, i32)> = Vec::new();
         let mut disjoint_sets: Vec<i32> = (0..self.n).collect();
 
-        for edge in self.edges {
-            if Self::find_set(edge.from, &disjoint_sets) != Self::find_set(edge.to, &disjoint_sets) {
-                mst.push(edge);
+        for edge in &self.edges {
+            if Self::find_set(edge.from, &mut disjoint_sets) != Self::find_set(edge.to, &mut disjoint_sets) {
+                mst.push((edge.from, edge.to));
                 Self::union_sets(edge.from, edge.to, &mut disjoint_sets);
             }
         }
 
         mst
+    }
+
+    fn find_set(x: i32, disjoint_sets: &mut Vec<i32>) -> i32 {
+        let x2 = x as usize;
+        if disjoint_sets[x2] != x {
+            disjoint_sets[x2] = Self::find_set(disjoint_sets[x2], disjoint_sets);
+        }
+        disjoint_sets[x2]
+    }
+    
+    fn union_sets(x: i32, y: i32, disjoint_sets: &mut Vec<i32>) {
+        let root_x = Self::find_set(x, disjoint_sets);
+        let root_y = Self::find_set(y, disjoint_sets);
+        disjoint_sets[root_x as usize] = root_y;
     }
 }
