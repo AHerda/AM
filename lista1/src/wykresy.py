071b2a7cec7b22de58cceb0fd4657a9d2e61b2ea
AHerda <adrianherda@gmail.com>
2023-10-08 23:39:03 +0200

    Poprawki

--- lista1/src/wykresy.py
+++ lista1/src/wykresy.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 headers = ["nazwa pliku", "MST size", "MST dfs size", "min from 1000", "avg min from 50", "avg min from 10"]
-dane = pd.read_csv("./dane.csv", sep=";", encoding="UTF-16LE", index_col=0, names=headers).transpose()
+dane = pd.read_csv("./plots/dane.csv", sep=";", encoding="UTF-16LE", index_col=0, names=headers).transpose()
 
 dane.plot(linestyle="-", marker=".")
 plt.xticks(rotation = 15)
