735e07228e886345bff59da84d97132bd05a35d5
AHerda <adrianherda@gmail.com>
2023-11-20 22:09:16 +0100

    Działą coraz szybciej

--- lista2/src/libs/graph_ext.rs
+++ lista2/src/libs/graph_ext.rs
@@ -4,8 +4,11 @@ use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 pub trait Lista2 {
     fn dfs_random_start(&self) -> Option<(Vec<i32>, i32)>;
-    fn invert(&self, path: &mut [i32], size: &mut i32, from: i32, to: i32);
+    fn invert(&self, path: &mut [i32], from: i32, to: i32) -> i32;
     fn measure_path(&self, path: &[i32]) -> i32;
+    fn all_pairs(&self) -> Vec<(i32, i32)>;
+    fn best_pair(&self, path: &Vec<i32>, all_pairs: &mut Vec<(i32, i32)>) -> ((i32, i32), i32);
+    fn local_search(&self, path: &mut Vec<i32>) -> (i32, i32);
 }
 
 impl Lista2 for Graph {
@@ -18,7 +21,7 @@ impl Lista2 for Graph {
                 let mut visited: Vec<bool> = vec![false; (self.size() + 1) as usize];
                 let mut number_visited = 0;
                 let mut times_visited: Vec<i32> = vec![0; (self.size() + 1) as usize];
-
+                println!("Dupa_dfs");
                 _ = self.dfs_traverse(
                     None,
                     (rng.gen::<u32>() % self.size() as u32 + 1) as i32,
@@ -34,24 +37,61 @@ impl Lista2 for Graph {
         }
     }
 
-    fn invert(&self, path: &mut [i32], size: &mut i32, from_id: i32, to_id: i32) {
-        let temp = path[from_id as usize];
-        path[from_id as usize] = path[to_id as usize];
-        path[to_id as usize] = temp;
+    fn local_search(&self, path: &mut Vec<i32>) -> (i32, i32) {
+        println!("Dupa local");
+        let mut it = 0;
+        let mut all_pairs = self.all_pairs();
+        let mut best_path_size = self.measure_path(path);
+        println!("Dupa local 2");
+        loop {
+            println!("it: {}\n\tsize: {}", it, best_path_size);
+            let (best_pair, size) = self.best_pair(&path.clone(), &mut all_pairs);
+            if size >= best_path_size {
+                return (best_path_size, it);
+            }
+            best_path_size = size;
+            self.invert(path, best_pair.0, best_pair.1);
+            it += 1;
+        }
+    }
 
-        *size = self.measure_path(&path);
+    fn all_pairs(&self) -> Vec<(i32, i32)> {
+        let mut result = Vec::new();
+        let n = self.size();
+
+        for i in 0..n {
+            for j in (i + 1)..n {
+                result.push((i, j));
+            }
+        }
+        result
+    }
+
+    fn best_pair(&self, path: &Vec<i32>, all_pairs: &mut Vec<(i32, i32)>) -> ((i32, i32), i32) {
+        println!("Dupa best pair\t{}", all_pairs.len());
+        let all_pairs2 = all_pairs
+            .par_iter()
+            .map(|(a, b)| ((*a, *b), self.invert(&mut path.clone(), *a, *b)))
+            .collect::<Vec<((i32, i32), i32)>>();
+        println!("Dupa best pair2");
+        *all_pairs2
+            .iter()
+            .min_by(|a, b| a.1.cmp(&b.1))
+            .expect("Da_fuq?!?")
+    }
+
+    fn invert(&self, path: &mut [i32], from_id: i32, to_id: i32) -> i32 {
+        path.swap(from_id as usize, to_id as usize);
+        self.measure_path(&path)
     }
 
     fn measure_path(&self, path: &[i32]) -> i32 {
         let mut size = 0;
         let len = path.len();
+        let neighbors = self.get_neighbors2();
 
         for i in 0..len {
-            size += self.get_neighbors()[path[i] as usize]
-                .par_iter()
-                .find_first(|e| e.to == path[(i + 1) % len])
-                .expect("No edge?!")
-                .weight;
+            size += neighbors[path[i] as usize][path[(i + 1) % len] as usize];
         }
 
         size
--- lista2/src/main.rs
+++ lista2/src/main.rs
@@ -1,17 +1,45 @@
 mod libs;
 
 use graph_tsp::graph::Graph;
-use libs::{help, graph_ext::Lista2};
-use rayon::iter::{IntoParallelRefIterator, IntoParallelRefMutIterator, ParallelIterator};
+use libs::{graph_ext::Lista2, help};
+use rayon::iter::{IntoParallelRefMutIterator, ParallelIterator};
 
 fn main() {
     let mut v: Vec<Graph> = help::read_dir("./data");
+    println!("graph_size;mst_size;avg_size;avg_steps;min_size");
 
-    v.par_iter_mut().for_each(|g| { g.mst(); });
-    // v.sort_by(|g1, g2| {g1.size().cmp(&g2.size())});
+    v[0].mst();
+    println!("Dupa");
+    let x = v[0].local_search(&mut v[0].dfs_random_start().unwrap().0);
+    println!("Dupa2");
+    // v.par_iter_mut().for_each(|g| {
+    //     let mst_size = g.mst();
+    //     let mut min_size = i32::MAX;
+    //     let mut avg_size = 0;
+    //     let mut avg_it = 0;
+    //     let n_sqrt = (g.size() as f64).sqrt();
+    //     for i in 0..(n_sqrt.floor() as i32) {
+    //         let (mut path, _size) = g.dfs_random_start().expect("???!!!");
+    //         let (ls_size, ls_it) = g.local_search(&mut path);
 
-    v.par_iter().for_each(|g| {
-        let (path, size) = g.dfs_random_start().expect("???!!!");
-        println!("nodes = {}\n\tsize = {}", g.size(), size);
-    });
+    //         avg_size += ls_size;
+    //         avg_it += ls_it;
+
+    //         if ls_size < min_size {
+    //             min_size = ls_size;
+    //         }
+    //         println!("size: {mst_size}\n\titer: {i}\n")
+    //     }
+
+    //     println!(
+    //         "{};{};{};{};{}",
+    //         g.size(),
+    //         mst_size,
+    //         avg_size as f64 / n_sqrt,
+    //         avg_it as f64 / n_sqrt,
+    //         min_size
+    //     );
+    // });
+
+    print!("{}, {}", x.0, x.1);
 }
